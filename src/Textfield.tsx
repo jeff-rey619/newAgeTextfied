dc2ff5be604ecda2c50df4dd82de249e6e0d8e8e
jeff rey <jeff-rey619>
2023-12-25 09:06:06 +0530

    basic css

---
+++ src/Textfield.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './textfield.css';
+
+function TextfieldComp() {
+    return (
+        <div className="textfield">
+            <input className="Christmas-Input-box" style={{ width:"100%" }} />
+        </div>
+    );
+}
+
+export default TextfieldComp;
